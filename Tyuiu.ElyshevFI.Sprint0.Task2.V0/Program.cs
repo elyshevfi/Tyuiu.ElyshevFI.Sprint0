573159aa03ed93a041e2c5e4ec04cec134a7a2ec
Tyuiu.ElyshevFI@outlook.com <Tyuiu.ElyshevFI@outlook.com>
2026-09-11 21:13:16 +0500

    Task2

---
+++ Tyuiu.ElyshevFI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ElyshevFI.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ElyshevFI.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+        }
+    }
+}
